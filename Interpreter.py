e7d1e4da1fa528a00528109076c7f1079e00f89c
IfElifElse <carlthechimchar#gmail.com>
2020-07-28 10:05:00 -0400

    block start checker no longer breaks when there's a space after 'start'

--- Interpreter.py
+++ Interpreter.py
@@ -8,11 +8,9 @@ from Track import Track
 
 class BSMLException(Exception):
     """ Generic exception class to specify an exception while interpreting. """
-    pass
-
 
 class BSMLInterpreter:
-    """ An interpreter for the Beat Saber markup language. """
+    """ The interpreter for the Beat Saber markup language. """
     def __init__(self, text):
         # Text to interpret.
         self.text = text
@@ -22,7 +20,9 @@ class BSMLInterpreter:
         for semicsplit in text.split(";"):
             self.lines += semicsplit.split("\n")
         # Each argument block in the text as a list of lines mapped to the line they start on.
-        self.blocks = {}
+        self.blocks = []
+        # A list of line numbers to be ignored on the second pass.
+        self.ignoreLines = []
         # A list of created Tracks.
         self.tracks = {}
         # The last-used track name.
@@ -36,7 +36,7 @@ class BSMLInterpreter:
         """ Run the text given to the interpreter. """
         
         # Current block start line index
-        cBlockStart = None
+        addLineToBlock = False
         
         # Find all the comments, "start" and "end" keywords, and remove them.
         # When a "start" keyword is found, set the current block start to the current line and start adding subsequent lines to a list of lines in .blocks.
@@ -45,57 +45,58 @@ class BSMLInterpreter:
         # Iterate through .lines (without a for loop because we're altering .lines)
         while self.ptr < len(self.lines):
             # Current line
-            line = self.lines[self.ptr]
+            line = self.lines[self.ptr].strip()
             # Find a comment or blank line.
-            if line.strip().startswith("#") or not line.strip():
-                self.lines.pop(self.ptr)
-                self.ptr -= 1
+            if line.startswith("#") or not line.strip():
+                self.ignoreLines.append(self.ptr)
             # Find a "start" keyword.
             elif line.endswith("start"):
                 self.lines[self.ptr] = line.replace("start", "")
-                cBlockStart = self.ptr
-                self.blocks[cBlockStart] = []
+                addLineToBlock = True
+                self.blocks.append([])
             # Find an "end" keyword.
             elif line.endswith("end"):
                 self.lines[self.ptr] = line.replace("end", "")
                 if not self.lines[self.ptr].strip():
-                    self.lines.pop(self.ptr)
-                    self.ptr -= 1
-                cBlockStart = None
+                    self.ignoreLines.append(self.ptr)
+                addLineToBlock = False
             # Find a line in between a "start" and an "end" keyword.
-            elif cBlockStart != None:
-                self.lines.pop(self.ptr)
-                self.ptr -= 1
-                self.blocks[self.ptr].append(line)
+            elif addLineToBlock:
+                self.ignoreLines.append(self.ptr)
+                self.blocks[-1].append(line)
             
             self.ptr += 1
         
-        # Execute based on the remaining non-block lines. Keywords documented in the conditional string.
-        self.ptr = 0
+        # Execute based on the remaining non-block lines. Keywords documented in the conditionals below.
         # Again, iterate through .lines (no alterations this time)
-        for line in self.lines:
+        for ptr, line in enumerate(self.lines):
+            if ptr in self.ignoreLines: continue
             # Each word in the line.
             tokens = [tok.strip() for tok in line.strip().split(" ")]
-            # Operator keyword to be matched in the conditional string.
+            # Operator keyword to be matched in the conditional statements below.
             op = tokens[0]
             # Each argument to the operator, supplied to the function called for the operator.
             args = tokens[1:]
-            #print("%s: %s" % (op, args))
+            print("executing %s: %s" % (op, args))
             
-            # Create a new track, with a specified blueprint and a name.
-            if op == "track":
-                self.newTrack(blueprint=args[0], name=args[1])
-            # Define a new plan, for a specified track (by name) and based on a preexisting plan if it exists.
-            elif op == "define":
-                self.define(name=args[0], block=self.blocks[self.ptr], fromPlan=None if len(args) <= 2 else args[2])
-            # Create a new structure, based on a specified track's plan.
-            elif op == "create":
-                self.create(name=args[0], beat=args[1])
-            # Take a list of tracks' plans and create a super-plan, with a given name.
-            elif op == "merge":
-                self.merge(name=args[0], block=self.blocks[self.ptr])
-            
-            self.ptr += 1
+            try:
+                # Create a new track, with a specified blueprint and a name.
+                if op == "track":
+                    self.newTrack(blueprint=args[0], name=args[1])
+                # Define a new plan, for a specified track (by name) and based on a preexisting plan if it exists.
+                elif op == "define":
+                    self.define(name=args[0], block=self.blocks.pop(0), fromPlan=None if len(args) <= 2 else args[2])
+                # Create a new structure, based on a specified track's plan.
+                elif op == "create":
+                    self.create(name=args[0], beat=args[1])
+                # Take a list of tracks' plans and create a super-plan, with a given name.
+                elif op == "merge":
+                    self.merge(name=args[0], block=self.blocks.pop(0))
+                elif op == "say":
+                    print("BSML: ".join(args))
+            except Exception as e:
+                print("Exception occurred while running BSML. Line %s: \n%s\n" % (ptr+1, line))
+                raise e
     
     
     def splitNames(self, name):
@@ -160,17 +161,19 @@ class BSMLInterpreter:
         self.lastTrack, self.lastPlan = self.splitNames(name)
         #print("merge start for %s's %s" % (self.lastTrack, self.lastPlan))
         planOffsets = {}
+        print("Interpreter merge:")
         # Parse the lines containing the plans we want to merge.
         for line in block:
             line = line.strip()
             # The name of the plan will be followed by the beat offset at which it will be placed in relation to the create beat, like "piano:hit at 0"
             splitline = [v.strip() for v in line.split(":")]
+            print("  splitline: %s" % splitline)
             beat = float(splitline[0])
             #print(splitline)
             mergeTrack, mergePlan = self.splitNames(":".join(splitline[1:]))
             #print("merging track %s's %s" % (mergeTrack, paramSetName))
             if not self.tracks[mergeTrack].hasPlan(mergePlan):
-                raise BSMLException("Track `%s` has no defined param set `%s`" % (mergeTrack, mergePlan))
+                raise BSMLException("Track `%s` has no defined plan `%s`" % (mergeTrack, mergePlan))
             # Set the given plan to the given beat
             planOffsets[beat] = self.tracks[mergeTrack].getPlan(mergePlan)
         
